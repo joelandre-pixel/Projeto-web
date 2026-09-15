3eca479e15eb54d900d1405e8d084fffbd129ee5
joelandre-pixel <joel.andre@escolar.ifrn.edu.br>
2026-09-15 09:24:53 -0300

    Add files via upload

---
+++ for numero in range.py
@@ -0,0 +1,15 @@
+for numero in range(1 ,6):
+     print(numero)
+
+if n >. 0:
+     print("positivo")
+     elif n 6 .<print("negativo")
+     numero = int(input("digite um número: "))
+
+     for contador in range (1 ,11):
+          resultado = numero * contador
+
+          print(numero,"X" , contador , "=" , resultado)
+           
+           for numero in range(50, 4 ,-5):
+               print(numero)
